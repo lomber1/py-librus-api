93c789b953d57e75258e284493900c361c9b7f47
Tomasz Nieżurawski <tomek.niezurawski@gmail.com>
2018-10-15 22:40:31 +0200

    Added teachers, comments and more to the grades dictionary

--- librus.py
+++ librus.py
@@ -1,105 +1,113 @@
 import requests
-from unidecode import unidecode
+from pprint import pprint
+from getpass import getpass
 
 
 class Librus:
-	host = "https://api.librus.pl/"
-	headers = {
-		"Authorization": "Basic Mjg6ODRmZGQzYTg3YjAzZDNlYTZmZmU3NzdiNThiMzMyYjE="
-	}
+    host = "https://api.librus.pl/"
+    headers = {
+        "Authorization": "Basic Mjg6ODRmZGQzYTg3YjAzZDNlYTZmZmU3NzdiNThiMzMyYjE="
+    }
 
-	grades = {}
-	subjects = {}
-	categories = {}
+    grades = {}
+    subjects = {}
+    categories = {}
+    teachers = {}
 
-	def __init__(self):
-		self.make_connection()
+    def __init__(self):
+        self.make_connection()
 
-	# Make connection and get access token
-	def make_connection(self):
-		r = requests.post(self.host + "OAuth/Token", data={"username": str(input("Login: ")),
-														   "password": str(input("Hasło: ")),
-														   "librus_long_term_token": "1",
-														   "grant_type": "password"},
-						  headers=self.headers)
+    # Make connection and get access token
+    def make_connection(self):
+        r = requests.post(self.host + "OAuth/Token", data={"username": str(input("Login: ")),
+                                                           "password": str(getpass(prompt="Hasło: ")),
+                                                           "librus_long_term_token": "1",
+                                                           "grant_type": "password"},
+            headers=self.headers)
 
-		self.headers["Authorization"] = "Bearer " + r.json()["access_token"]
+        self.headers["Authorization"] = "Bearer " + r.json()["access_token"]
 
-	def get_lucky_number(self):
-		r = requests.get(self.host + "2.0/LuckyNumbers", headers=self.headers)
-		print("Szczęśliwy numerek: " + str(r.json()["LuckyNumber"]["LuckyNumber"]))
+    def get_lucky_number(self):
+        r = requests.get(self.host + "2.0/LuckyNumbers", headers=self.headers)
+        print("Szczęśliwy numerek: " + str(r.json()["LuckyNumber"]["LuckyNumber"]))
 
-	def get_grades(self):
-		r = requests.get(self.host + "2.0/Grades", headers=self.headers)
+    def get_grades(self):
+        r = requests.get(self.host + "2.0/Grades", headers=self.headers)
 
-		if not self.subjects:
-			self.get_subjects()
+        if not self.subjects:
+            self.get_subjects()
 
-		if not self.categories:
-			self.get_categories()
+        if not self.categories:
+            self.get_categories()
 
-		self.grades = {
+        if not self.teachers:
+            self.get_teachers()
 
-		}
+        self.grades = {i: [] for i in self.subjects.values()}
+        grades_comments = self.get_comments()
 
-		"""
-		grades format:
-		
-		grades = {
-			"Subject": [
-				{
-					"Grade": "",
-					"Weight": "",
-					"Teacher": "",
-					"Category": "",
-				},
-			],
-		}
-		"""
+        for i in r.json()["Grades"]:
+            if "Comments" in i:
+                comment = grades_comments[i["Comments"][0]["Id"]]["Text"]
+            else:
+                comment = "Brak komentarza"
 
-		for i in r.json()["Grades"]:
-			try:
-				self.grades[self.subjects[i["Subject"]["Id"]]].append({
-					"Ocena": i["Grade"],
-					"Waga": self.categories[i["Category"]["Id"]]["Weight"],
-					"Kategoria": self.categories[i["Category"]["Id"]]["Name"],
-				})
-			except KeyError:
-				self.grades[self.subjects[i["Subject"]["Id"]]] = [{
-					"Ocena": i["Grade"],
-					"Waga": self.categories[i["Category"]["Id"]]["Weight"],
-					"Kategoria": self.categories[i["Category"]["Id"]]["Name"],
-				}]
+            self.grades[self.subjects[i["Subject"]["Id"]]].append({
+                "Ocena": i["Grade"],
+                "Waga": self.categories[i["Category"]["Id"]]["Weight"],
+                "Kategoria": self.categories[i["Category"]["Id"]]["Name"],
+                "Nauczyciel": "%s %s" % (self.teachers[i["AddedBy"]["Id"]]["FirstName"],
+                                         self.teachers[i["AddedBy"]["Id"]]["LastName"]),
+                "Komentarz": comment,
+                "Do średniej": self.categories[i["Category"]["Id"]]["CountToTheAverage"]
+            })
 
-		print(self.grades)
+        pprint(self.grades)
 
-	def get_subjects(self):
-		r = requests.get(self.host + "2.0/Subjects", headers=self.headers)
+    def get_subjects(self):
+        r = requests.get(self.host + "2.0/Subjects", headers=self.headers)
 
-		self.subjects = {i["Id"]: i["Name"] for i in r.json()["Subjects"]}
+        self.subjects = {i["Id"]: i["Name"] for i in r.json()["Subjects"]}
 
-	def get_categories(self):
-		r = requests.get(self.host + "2.0/Grades/Categories", headers=self.headers)
+    def get_categories(self):
+        r = requests.get(self.host + "2.0/Grades/Categories", headers=self.headers)
 
-		for i in r.json()["Categories"]:
-			try:
-				self.categories[i["Id"]] = {
-						"Name": i["Name"],
-						"Weight": i["Weight"],
-						"CountToTheAverage": i["CountToTheAverage"],
-					}
+        w = None
+        for i in r.json()["Categories"]:
+            if "Weight" in i:
+                w = i["Weight"]
+            else:
+                w = None
 
-			except KeyError:
-				self.categories[i["Id"]] = {
-						"Name": i["Name"],
-						"Weight": None,
-						"CountToTheAverage": i["CountToTheAverage"],
-					}
+            if i["CountToTheAverage"]:
+                i["CountToTheAverage"] = "Tak"
+            else:
+                i["CountToTheAverage"] = "Nie"
 
-	def get_lessons(self):
-		r = requests.get(self.host + "2.0/Lessons", headers=self.headers)
+            self.categories[i["Id"]] = {
+                "Name": i["Name"],
+                "Weight": w,
+                "CountToTheAverage": i["CountToTheAverage"],
+            }
 
-	def get_teachers(self):
-		r = requests.get(self.host + "2.0/Users", headers=self.headers)
+    def get_teachers(self):
+        r = requests.get(self.host + "2.0/Users", headers=self.headers)
 
-		[print(str(teacher["Id"]) + ". " + teacher["FirstName"] + " " + teacher["LastName"]) for teacher in r.json()["Users"]]
+        self.teachers = {
+            i["Id"]: {
+                "FirstName": i["FirstName"],
+                "LastName": i["LastName"]
+            } for i in r.json()["Users"]
+        }
+
+    def get_comments(self):
+        r = requests.get(self.host + "2.0/Grades/Comments", headers=self.headers)
+
+        return {
+            i["Id"]: {
+                "Text": i["Text"]
+            } for i in r.json()["Comments"]
+        }
+
+    def get_lessons(self):
+        r = requests.get(self.host + "2.0/Lessons", headers=self.headers)
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from librus import Librus
 
 if __name__ == "__main__":
-	librus = Librus()
-	librus.get_lucky_number()
-	librus.get_grades()
-
+    librus = Librus()
+    librus.get_lucky_number()
+    librus.get_grades()
